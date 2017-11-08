b148ba5f3eb33afa6268659e8b0a257cc4e34b9f
Kyle Baker <k@gnomiq.com>
2017-11-08 12:08:52 -0800

    Added component template

---
+++ packages/retrade/lib/components/ComponentTemplate.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+export default class ComponentTemplate extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+        )
+    }
+}
